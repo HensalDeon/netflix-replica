39c901915b77cf850fcbce8e7566fe4c56a0005f
hensal <hensaldeon777@gmail.com>
2023-08-30 09:11:04 +0530

    update path

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
             <Router>
                 <Routes>
                     <Route
-                        path="/netflix-replica"
+                        path="/"
                         element={
                             <React.Fragment>
                                 <Header />
